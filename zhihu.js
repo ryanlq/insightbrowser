4724be81229f5055a55fbab9f29d0ed9e9be1f68
ryan <ryan.lq@hotmail.com>
2021-04-05 19:16:22 +0800

    Add files via upload

--- zhihu.js
+++ zhihu.js
@@ -1,13 +1,4 @@
-// ==UserScript==
-// @name         知乎免登陆 - 新版
-// @description  知乎免登陆
-// @namespace    https://www.zhihu.com/
-// @match        *://www.zhihu.com/*
-// @version 0.0.1.20201201064337
-// ==/UserScript==
 
-'use strict';
-(function () {
     document.getElementsByClassName('OpenInAppButton')[0].hidden = true
     document.getElementsByClassName('ModalWrap')[0].hidden= true
     document.getElementsByTagName('Header')[0].hidden=true
@@ -15,4 +6,3 @@
     document.getElementsByClassName('AnswersNavWrapper')[0].nextSibling.remove()
     document.getElementsByClassName('HotQuestions')[0].remove()
     document.getElementsByClassName('AdBelowMoreAnswers')[0].remove()
-})();
